0c1e16c0141d1f22d62ca700bd4d017c9e4ade59
Karen Lundgren <karen.e.lundgren@gmail.com>
2013-09-20 12:44:17 -0600

    Attempting to fix errors with dropping when no db exists.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,7 +1,12 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying the integrity of signed cookies.
+# Your secret key is used for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
+
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-RailsApp::Application.config.secret_key_base = '025ada0c49de0bc31a3b5fe59dadeb61f954aa27eadd63731d6113be7e62657025167618dfbe9f4887d76670f6ed7636907596eca83c9bdbee28129fd9e388d2'
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsApp::Application.config.secret_key_base = '5bed8f1ff63d01046357cc0845213ff28049ad0f4b154c633f1b8ff81064852fa6c97dbf7dd08267e2123a0520cc3b6a5be68a01b56ad60f4daf4fc89e0348f6'
